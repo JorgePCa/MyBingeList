b08a56e4983c9d0141089e82d4d1c23356f46bc3
NielsPe <96286464+NielsPe@users.noreply.github.com>
2022-10-10 20:09:08 +0200

    Ajout td

--- Front/task-manager/src/App.svelte
+++ Front/task-manager/src/App.svelte
@@ -1,17 +1,47 @@
 <script>
-	export let name;
+  import ToDoInputForm from "./ToDoInputForm.svelte"
+  import ToDoList from "./ToDoList.svelte"
+  let nameEntered = false
+
+  let Name = ""
+  let Email = ""
+  let Mdp = ""
+
+  function handleSubmit() {
+  if (Name && Email && Mdp) {
+    nameEntered = true
+  }
+}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+{#if nameEntered}
+    <h1>Welcome to task manager, {Name}!</h1>
+    <ToDoInputForm userName={Name} />
+    <ToDoList />
+{:else}
+	<h1>Connectez vous pour accéder à votre liste</h1>
+  	<h3>Entrez vos informations</h3>
+	  <form on:submit|preventDefault={handleSubmit}>
+  	<div>
+    	<input bind:value={Name} type="text" placeholder="Nom" required>
+  	</div>
+	<div>
+    	<input bind:value={Email} type="text" placeholder="Email" required>
+  	</div>
+  	<div>
+    	<input bind:value={Mdp} type="password" placeholder="Mot de Passe"  required>
+  	</div>
+	<button on:click={handleSubmit}>Commencez à gérer les tâches</button>
+</form>
+{/if}
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		max-width: 450px;
 		margin: 0 auto;
 	}
 
---
+++ Front/task-manager/src/ToDoInputForm.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { toDoItems } from "./store.js"
+    import { onMount } from "svelte"
+  
+    export let userName
+    let newItem = ""
+    let newItemInputForm = null
+  
+    onMount(() => {
+      newItemInputForm.focus()
+    })
+  
+    function addToList() {
+      $toDoItems = [...$toDoItems, {text: newItem, status: false}]
+      newItem = ""
+    }
+</script>
+  
+<input bind:value={newItem} bind:this={newItemInputForm} type="text" placeholder="Ajouter une nouvelle tâche">
+<button on:click={addToList}>Ajouter</button>
+  
+  
+<h3>Liste de tâches de {userName}:</h3>
