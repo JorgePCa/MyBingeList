e1c9251c76290e370a9be999644ed19052aa1f9d
Jorge CARREGUEIRA <jorgemaraduh@gmail.com>
2022-10-17 13:13:36 +0200

    bdd v1.0

--- db/mybingelist.sql
+++ db/mybingelist.sql
@@ -1,47 +1,74 @@
-CREATE TABLE users
+CREATE TABLE "users"
 (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100),
-    mail VARCHAR(255),
-    birth_date DATE,
-    password STRING,
+    "id"            serial  NOT NULL,
+    "name"          text    NOT NULL,
+    "mail"          text    NOT NULL,
+    "birth_date"    DATE    NOT NULL,
+    "password"      text    NOT NULL,
+    PRIMARY KEY("id"),
+    UNIQUE ("name","mail")
 );
 
-ALTER TABLE users
-ADD UNIQUE (id,name,mail);
-
-CREATE TABLE status
+CREATE TABLE "status"
 (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    definition VARCHAR(255),
-    designation VARCHAR5(255),
+    "id"            serial  NOT NULL,
+    "definition"    text    NOT NULL,
+    "designation"   text    NOT NULL,
+    PRIMARY("id"),
+    UNIQUE ("def","designation")
 );
 
-ALTER TABLE status
-ADD UNIQUE (def,designation)
-
-CREATE TABLE type
+CREATE TABLE "type"
 (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    definition VARCHAR(255),
-    designation VARCHAR5(255),
+    "id"            serial  NOT NULL,
+    "definition"    text    NOT NULL,
+    "designation"   text    NOT NULL,
+    
+    UNIQUE ("def","designation")
 );
 
-ALTER TABLE type
-ADD UNIQUE (def,designation)
-
-CREATE TABLE categories
+CREATE TABLE "categories"
 (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    definition VARCHAR(255),
-    designation VARCHAR5(255),
+    "id"            serial  NOT NULL,
+    "definition"    text    NOT NULL,
+    "designation"   text    NOT NULL,
+    PRIMARY("id"),
+    UNIQUE ("def","designation")
 );
 
-ALTER TABLE categories
-ADD UNIQUE (def,designation)
-
-CREATE TABLE media
+CREATE TABLE "media"
 (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    fk_type INT, 
+    "id"                    serial  NOT NULL,
+    "fk_type"               integer NOT NULL, 
+    "title"                 text    NOT NULL,
+    "season_volume"         text    NOT NULL,
+    "fk_category"           text    NOT NULL,
+    "year"                  integer NOT NULL,
+    "author_director"       text,
+    "nb_episodes_chapters"  integer,
+    "score"                 real,  
+    PRIMARY("id"),
+    UNIQUE ("title","type","year","season_volume")
 );
+
+CREATE TABLE "userMedia"
+(
+    "id"            serial      NOT NULL,
+    "fk_iduser"     integer     NOT NULL,
+    "fk_idmedia"     integer     NOT NULL,
+    "fk_status"     integer     NOT NULL,
+    "hd_creation"   date        DEFAULT Now(),
+    "active"        boolean     DEFAULT TRUE,
+    PRIMARY("id"),
+    UNIQUE("fk_iduser","fk_idmedia","hd_creation")
+);
+
+ALTER TABLE "media" ADD FOREIGN KEY ("fk_type") REFERENCES "type"("id");
+
+ALTER TABLE "media" ADD FOREIGN KEY ("fk_category") REFERENCES "category"("id");
+
+ALTER TABLE "userMedia" ADD FOREIGN KEY ("fk_iduser") REFERENCES "user"("id");
+
+ALTER TABLE "userMedia" ADD FOREIGN KEY ("fk_idmedia") REFERENCES "media"("id");
+
+ALTER TABLE "userMedia" ADD FOREIGN KEY ("fk_status") REFERENCES "status"("id");
