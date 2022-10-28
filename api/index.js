8819dd5bd825addce9b55c30777b1612aeaec286
JorgePCa <jorgem.carregueira@gmail.com>
2022-10-28 13:58:59 +0200

    db crud

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const users = require('./users');
+const media = require('./media');
+const statusW = require('./statusW');
+const type = require('./type');
+const category = require('./category');
+const userMedia = require('./userMedia');
+
+module.exports = app => {
+  app.use('/users', users);
+  app.use('/media', media);
+  app.use('/statusW', statusW);
+  app.use('/type', type);
+  app.use('/category', category);
+  app.use('/userMedia', userMedia);
+};
