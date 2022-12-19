0b4fdb1db31b73c0c1dff2629882f2afbb9cfde6
Jorge CARREGUEIRA <jorgemaraduh@gmail.com>
2022-12-19 12:17:44 +0100

    [BRANCH] Start Routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,11 @@
 <script>
-    import RegisterForm from "./RegisterForm.svelte";
-
-   
-
-	let nameEntered = false
-
-	let createAccount = false
-
+    import RegisterForm from "./pages/RegisterForm.svelte";
+	import Home from "./pages/Home.svelte";
+	import router from 'pages';
 	
-	let research = ""
+	let page;
+	let params;
 
-	function Find() {
 
-	}
-	function CreateAccountPage(){
-
-    	createAccount = true
-	}
-	function handleSubmit() {
-  	if (userName && Email && password) {
-    	nameEntered = true
- 	 }
-	}
-  </script>
+</script>
   
-  <main>
-
-
-	{#if createAccount}
-	  <RegisterForm/>
-	{:else}
-	<input bind:value={research} type="text" placeholder="Anime, catégorie, ...">
-	<button on:click={Find}>Rechercher</button>
-	<button on:click={CreateAccountPage}>Créez un compte</button>
-
-	{/if}
-	
-  </main>
-  
-  
-  <style>
-	main {
-	  text-align: center;
-	  padding: 1em;
-	  max-width: 450px;
-	  margin: 0 auto;
-	}
-  </style>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,35 @@
+<script>
+    let research = ""
+	let nameEntered = false
+
+
+
+
+
+
+	function Find() {
+
+	}	
+	function CreateAccountPage(){
+		
+	}
+
+
+</script>
+<main>
+
+	<input bind:value={research} type="text" placeholder="Anime, catégorie, ...">
+	<button on:click={Find}>Rechercher</button>
+	<button on:click={CreateAccountPage}>Créez un compte</button>
+
+</main>
+  
+  
+<style>
+    main {
+	  text-align: center;
+	  padding: 1em;
+	  max-width: 450px;
+	  margin: 0 auto;
+	}
+</style>
