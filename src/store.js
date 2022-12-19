c7f4e9e9fe17d081cbce8b89e3dbaee254d2f334
Jorge CARREGUEIRA <jorgemaraduh@gmail.com>
2022-12-19 12:22:12 +0100

    [DELETE] Remake

--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { writable } from "svelte/store"
-
-export const toDoItems = writable([
-    {text: 'Réfléchir à un projet ', status: true},
-    {text: 'Proposer un sujet de projet', status: false},
-    {text: 'Envoyer le compte Github', status: false}
-])
