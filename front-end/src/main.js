81917e0d49fb0f18e815e1d0fd609899d6dbb842
Jorge CARREGUEIRA <jorgemaraduh@gmail.com>
2022-12-23 11:05:36 +0100

    syntax clean

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -5,15 +5,10 @@ import App from './App.vue'
 import { createAuth0 } from '@auth0/auth0-vue';
 
 
-const app = createApp(App);
-
-app.use(router)
-app.use(
+createApp(App).use(router).use(
     createAuth0({
       domain: "dev-ztxv7mlnzq07l4p2.us.auth0.com",
       client_id: "r0FAyZOlPqeu8axhLyYfi1yPmwqGpLkH",
       redirect_uri: window.location.origin
     })
-  );
-  
-app.mount('#app');
+  ).mount('#app');
--- front-end/src/plugins/router.js
+++ front-end/src/plugins/router.js
@@ -1,17 +1,37 @@
 import {createRouter, createWebHistory} from 'vue-router'
 
 const routes = [
-    {path : '',name:'home',component:()=>import('../views/Home.vue')},
-    {path : '/login',name:'login',component:()=>import('../views/LogIn.vue')},
-    {path : '/createaccount',name:'createAccount',component:()=>import('../views/CreateAccount.vue')},
-    {path : '/typebinge',name:'typeBinge',component:()=>import('../views/TypeBinge.vue')},
-    {path : '/myaccount',name:'myAccount',component:()=>import('../views/MyAccount.vue')}
+    {
+        path : '',
+        name:'home',
+        component:()=>import('../views/Home.vue')
+    },
+    {
+        path : '/login',
+        name:'login',
+        component:()=>import('../views/LogIn.vue')
+    },
+    {
+        path : '/createaccount',
+        name:'createAccount',
+        component:()=>import('../views/CreateAccount.vue')
+    },
+    {
+        path : '/typebinge',
+        name:'typeBinge',
+        component:()=>import('../views/TypeBinge.vue')
+    },
+    {
+        path : '/myaccount',
+        name:'myAccount',
+        component:()=>import('../views/MyAccount.vue')
+    }
 
-]
+];
 
 const router = createRouter({
+    history : createWebHistory(),    
     routes,
-    history : createWebHistory(),
 })
 
-export default router
+export default router;
